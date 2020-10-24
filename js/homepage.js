62f982fd5e9060d2a94509c8d9733eb127216f3e
guptstagram <sourabh.gupta.5095@gmail.com>
2020-10-24 21:59:08 +0530

    hero_added

--- js/homepage.js
+++ js/homepage.js
@@ -11,81 +11,86 @@ const movingHelloText = () => {
   }, 2000);
 };
 
-const addTextEffect=(target)=>{
+const addTextEffect = (target) => {
   target.classList.add("text-effect");
-  setTimeout(()=>target.classList.remove("text-effect"),800)
-}
+  setTimeout(() => target.classList.remove("text-effect"), 800);
+};
 
 // window.getComputedStyle( document.querySelector('.intro-text-name-font'), '::after' ).getPropertyValue("display")
 
 const increasingVisibilityofIntroText = () => {
-  const line1="I'M SOURABH";
-  const line2="FULL-STACK DEVELOPER";
-  const line3="I DEVELOP WEB & MOBILE APPS";
-  const line4="I stand on a fine spot where Design fuses into Code.";
-  let currTime=3000;
-  
+  const line1 = "I'M SOURABH";
+  const line2 = "FULL-STACK DEVELOPER";
+  const line3 = "I DEVELOP WEB & MOBILE APPS";
+  const line4 = "I stand on a fine spot where Design fuses into Code.";
+  let currTime = 3000;
+
   setTimeout(() => {
-    document.querySelector(".intro-text-name-font").classList.add("after-danda");
+    document
+      .querySelector(".intro-text-name-font")
+      .classList.add("after-danda");
   }, currTime);
-  for(let i=0;i<line1.length;i++){
+  for (let i = 0; i < line1.length; i++) {
     setTimeout(() => {
-      let span=document.createElement("span");
-      span.innerText=line1[i];
-      span.addEventListener("mouseover",()=>addTextEffect(span));
+      let span = document.createElement("span");
+      span.innerText = line1[i];
+      span.addEventListener("mouseover", () => addTextEffect(span));
       document.querySelector(".intro-text-name-font").appendChild(span);
       // document.querySelector(".intro-text-name-font").innerHTML+="<span onmouseover='addTextEffect(this)'>"+line1[i]+"</span>";
-    }, currTime+=80);
+    }, (currTime += 80));
   }
   setTimeout(() => {
-    document.querySelector(".intro-text-name-font").classList.remove("after-danda");
+    document
+      .querySelector(".intro-text-name-font")
+      .classList.remove("after-danda");
   }, currTime);
-
-
 
   setTimeout(() => {
-    document.querySelector(".intro-text-developer-font").classList.add("after-danda");
+    document
+      .querySelector(".intro-text-developer-font")
+      .classList.add("after-danda");
   }, currTime);
-  for(let i=0;i<line2.length;i++){
+  for (let i = 0; i < line2.length; i++) {
     setTimeout(() => {
-      document.querySelector(".intro-text-developer-font").innerHTML+="<span>"+line2[i]+"</span>";
-    }, currTime+=80);
+      document.querySelector(".intro-text-developer-font").innerHTML +=
+        "<span>" + line2[i] + "</span>";
+    }, (currTime += 80));
   }
   setTimeout(() => {
-    document.querySelector(".intro-text-developer-font").classList.remove("after-danda");
+    document
+      .querySelector(".intro-text-developer-font")
+      .classList.remove("after-danda");
   }, currTime);
-  
-
-
 
   setTimeout(() => {
-    document.querySelector(".intro-text-apps-font").classList.add("after-danda");
+    document
+      .querySelector(".intro-text-apps-font")
+      .classList.add("after-danda");
   }, currTime);
-  for(let i=0;i<line3.length;i++){
+  for (let i = 0; i < line3.length; i++) {
     setTimeout(() => {
-      document.querySelector(".intro-text-apps-font").innerHTML+="<span>"+line3[i]+"</span>";
-    }, currTime+=80);
+      document.querySelector(".intro-text-apps-font").innerHTML +=
+        "<span>" + line3[i] + "</span>";
+    }, (currTime += 80));
   }
   setTimeout(() => {
-    document.querySelector(".intro-text-apps-font").classList.remove("after-danda");
+    document
+      .querySelector(".intro-text-apps-font")
+      .classList.remove("after-danda");
   }, currTime);
 
-
-
   setTimeout(() => {
-      document.querySelector(".intro-text-quote-font").innerHTML="<q></q>";
-  }, currTime+=100);
-  for(let i=0;i<line4.length;i++){
+    document.querySelector(".intro-text-quote-font").innerHTML = "<q></q>";
+  }, (currTime += 100));
+  for (let i = 0; i < line4.length; i++) {
     setTimeout(() => {
-      document.querySelector(".intro-text-quote-font>q").innerHTML+=line4[i];
-    }, currTime+=50);
+      document.querySelector(".intro-text-quote-font>q").innerHTML += line4[i];
+    }, (currTime += 50));
   }
 };
 
-
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   increaseBlueColorWidth();
   movingHelloText();
   increasingVisibilityofIntroText();
-});  
+});
